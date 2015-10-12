20edf10d836563b850338241feb22c4186179d06
Jeremy Karlsson <hello@jeremy.se>
2015-10-12 19:29:10 +0200

    Updated /cprotect and /cpyramid

--- src/se/jeremy/minecraft/cuboid/commands/CProtectCommand.java
+++ src/se/jeremy/minecraft/cuboid/commands/CProtectCommand.java
@@ -1,6 +1,7 @@
 package se.jeremy.minecraft.cuboid.commands;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -14,26 +15,26 @@ import se.jeremy.minecraft.cuboid.CuboidC;
 
 public class CProtectCommand implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label,
-			String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
 		if (!(sender instanceof Player)) {
 			return true;
 		}
+		
 		Player player = (Player) sender;
-		String playerName = player.getName();
+		UUID playerId = player.getUniqueId();
+		
 		CuboidC playersArea = CuboidAreas.findCuboidArea(player.getLocation());
-		if (playersArea != null && !playersArea.isAllowed(args[0])
-				&& !playersArea.isOwner(player)
-				&& !player.hasPermission("cuboidplugin.ignoreownership")) {
-			player.sendMessage(ChatColor.RED
-					+ "This command is disallowed in this area");
+		
+		if (playersArea != null && !playersArea.isAllowed(cmd) && !playersArea.isOwner(player) && !player.hasPermission("cuboidplugin.ignoreownership")) {
+			player.sendMessage(ChatColor.RED + "This command is disallowed in this area");
 			return true;
 		}
 
-		if (CuboidAction.isReady(playerName, true)) {
+		if (CuboidAction.isReady(playerId, true)) {
 			ArrayList<String> ownersList = new ArrayList<String>();
 			int paramSize = args.length;
+			
 			if (paramSize > 1) {
 				for (short i = 1; i < paramSize - 1; i++) {
 					ownersList.add(args[i]);
@@ -41,12 +42,10 @@ public class CProtectCommand implements CommandExecutor {
 				String cuboidName = args[paramSize - 1].trim().toLowerCase();
 
 				// TODO
-				CuboidAreas.protectCuboidArea(player, ownersList, cuboidName,
-						false);
+				CuboidAreas.protectCuboidArea(player, ownersList, cuboidName, false);
 				return true;
 			} else {
-				player.sendMessage(ChatColor.YELLOW
-						+ "You need to specify at least one player or group, and a name.");
+				player.sendMessage(ChatColor.YELLOW + "You need to specify at least one player or group, and a name.");
 				return true;
 			}
 		} else {
